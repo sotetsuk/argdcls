8f4ccaeea8b4c390abc3c58cdf07159fd653ad08
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-12-11 05:33:38 +0900

    implement Config._parse

--- argdcls/config.py
+++ argdcls/config.py
@@ -15,4 +15,48 @@ class Config:
     def _parse(
         s: str,
     ) -> Tuple[str, str, Any]:
-        return "", "", None
+        s = s.strip().strip("\n")
+
+        # parse param_t
+        param_t = ""
+        if s.startswith("++"):
+            param_t = "++"
+            s = s[2:]
+        if s.startswith("+"):
+            param_t = "+"
+            s = s[1:]
+
+        # parse key
+        assert "=" in s
+        assert len(s.split("=")) == 2
+        key, val = s.split("=")
+
+        # parse val
+        if val == "None":
+            val = None
+        elif val == "True":
+            val = True
+        elif val == "False":
+            val = False
+        elif _is_num(val):
+            if _is_integer(val):
+                val = int(val)
+            else:
+                val = float(val)
+
+        assert param_t in ["", "+", "++"]
+        assert "=" not in key
+        return param_t, key, val
+
+
+def _is_num(n: str):
+    try:
+        float(n)
+        return True
+    except ValueError:
+        return False
+
+
+def _is_integer(n: str):
+    assert _is_num(n)
+    return float(n).is_integer()
--- tests/test_config.py
+++ tests/test_config.py
@@ -2,8 +2,20 @@ from argdcls.config import Config
 
 
 def test_parse():
-    # basic usage
+    # no +/++
+    param_t, key, val = Config._parse("lr=0.1")
+    assert param_t == ""
+    assert key == "lr"
+    assert val == 0.1
+
+    # +
     param_t, key, val = Config._parse("+lr=0.1")
-    assert param_t == "override"
+    assert param_t == "+"
+    assert key == "lr"
+    assert val == 0.1
+
+    # ++
+    param_t, key, val = Config._parse("++lr=0.1")
+    assert param_t == "++"
     assert key == "lr"
     assert val == 0.1
