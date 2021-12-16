73d2375b484a955a3addaf89680c900414fcd2e6
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-12-16 13:35:38 +0900

    add test

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 
+import pytest
+
 import argdcls
 from argdcls.config import _parse
 
@@ -31,6 +33,16 @@ def test_load_params():
     assert config.lr == 1.0
     assert config.adam
     assert config.addon == 3  # type: ignore
+
+
+def test_error_cases():
+    # raise value error if typo exists
+    with pytest.raises(Exception) as e:
+        _ = argdcls.load(Config, ["lr=1.0", "adm=True"])
+    assert (
+        str(e.value)
+        == "Parameter \"adm\" not in ['lr', 'adam']. You may use \"+adm=True\" instead."
+    )
 
 
 def test_parse():
